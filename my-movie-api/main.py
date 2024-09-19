fb19ad0db41f8b7f948f9153b74862b0da163de6
oscarmellizo <oscar@oscarmellizo.com>
2024-09-18 21:33:08 -0500

    Primeros ejemplos con GET
    Primeros ejemplos con GET

---
+++ my-movie-api/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+app.title = "Mi Aplicacion con FastAPI"
+app.version = "0.0.1"
+
+@app.get('/', tags=['home'])
+
+def message():
+    return "Hello World! :D"
